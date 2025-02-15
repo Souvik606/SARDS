c196543651f76a72c73f53b4f3af3c4fa1f879ee
rahulc0dy <rahulchakraborty337@gmail.com>
2025-02-15 16:57:37 +0530

    fix(loader): loader position.

--- frontend/app/loading.tsx
+++ frontend/app/loading.tsx
@@ -2,5 +2,9 @@ import { Loader } from "lucide-react";
 
 export default function Loading() {
   // Or a custom loading skeleton component
-  return <Loader className="size-10 animate-spin" />;
+  return (
+    <div className="flex items-center justify-center p-5">
+      <Loader className="size-10 animate-spin" />
+    </div>
+  );
 }
